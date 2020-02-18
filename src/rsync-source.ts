5cb267dc441ad307a881df3d32cfa18c1975e440
Danilo Castro <ccastro.dan@gmail.com>
2020-02-17 23:04:18 -0800

    Change to one way sync.

--- src/rsync-source.ts
+++ src/rsync-source.ts
@@ -7,34 +7,23 @@ var w: boolean = args.indexOf("--watch") > -1 || args.indexOf("-w") > -1;
 
 export async function move(source: string, dest: string, del: boolean = false) {
   return new Promise((resolve, reject) => {
-    term.eraseLine().column(0);
-    term.bold.green(' Syncing\t\t\t\t\t');
     exec(`rsync -rl --update ${del ? "--del" : ""} ${source} ${dest}`, (err: any, stdout: any, stderr: any) => {
-      term.eraseLine().column(0);
-      term.bold.green(' Watching\t\t\t\t\t').column(0);
       resolve();
     });
   });
 }
 export async function sync(source: string, dest: string, folder: string) {
+  term.bold.green(`sync: \t${source} ${dest}/${folder}`);
   if (!fs.existsSync(`${dest}/${folder}`)) { fs.mkdirSync(`${dest}/${folder}`); }
   return new Promise((res, rej) => {
     move(`${source}/${folder}`, dest, true).then(() => {
-      move(`${dest}/${folder}`, source, false)
-        .then(() => {
-          term.eraseLine().column(0);
-          term.bold.green(' Watching\t\t\t\t\t').column(0);
-          if (w) {
-            watch(`${source}/${folder}`, { recursive: true }, (evt: any, name: any) => {
-              move(`${source}/${folder}`, dest, true);
-            });
-            watch(`${dest}/${folder}`, { recursive: true }, (evt: any, name: any) => {
-              move(`${dest}/${folder}`, source);
-            });
-          } else {
-            res();
-          }
+      if (w) {
+        watch(`${source}/${folder}`, { recursive: true }, (evt: any, name: any) => {
+          move(`${source}/${folder}`, dest, true);
         });
+      } else {
+        res();
+      }
     });
   });
 }
